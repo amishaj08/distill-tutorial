d8c8ca35502d5794f54e38919553b2b4ed5a388c
amishaj08 <amishaj08@gmail.com>
2022-06-26 22:27:37 -0500

    finished verision

---
+++ make-car-plot.R
@@ -0,0 +1,9 @@
+car_plot <- mtcars  %>% select(mpg, wt)  %>% 
+  ggplot(aes(x = wt, y = mpg)) + geom_point() + 
+  geom_smooth(method = "lm", formula = y ~ x, se = FALSE) + 
+  theme_clean() + labs(title = "Fuel Efficiency and Weight", 
+                       subtitle = "Heavier cars are less fuel efficent", 
+                       x = "Weight (1000s of lbs)", 
+                       y = "MPG", caption = "Henderson and Vellerman(1981)")
+
+write_rds(car_plot, "car-plot.rds")
